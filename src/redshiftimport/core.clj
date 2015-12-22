7e5412272d95e3f95299a193757fb9babcdabdcd
Gerrit Jansen van Vuuren <gerritjvv@gmail.com>
2015-12-22 12:07:54 +0100

    update dist

--- src/redshiftimport/core.clj
+++ src/redshiftimport/core.clj
@@ -7,8 +7,17 @@
   (:import [java.util.concurrent Executors ExecutorService Future]
            [com.amazonaws.regions RegionUtils]
            [com.amazonaws.util StringInputStream]
-           [java.io InputStream]))
+           [java.io InputStream]
+           [org.apache.commons.lang StringUtils]))
 
+
+(defn remove-double-slashes [s]
+  (StringUtils/replace (str s) "//" "/"))
+
+(defn remove-s3-prefix [s]
+  (StringUtils/replace (str s) "s3:" ""))
+
+(def sanitise-s3-path (comp remove-s3-prefix remove-double-slashes))
 
 (defn hdfs-file->s3
   "hdfs-ctx: the hdfs context
@@ -18,28 +27,29 @@
    i: unique index that will be added to the file name, this is used incase multiple dirs are globbed and files have the same name
 
    return the s3bucket/filenameuploaded"
-  [hdfs-ctx hdfs-file s3-ctx s3bucket s3path i]
-  (let [file-name (str s3path "/" (hdfs/file-name hdfs-file) "_" i)
+  [hdfs-ctx hdfs-file s3-ctx s3bucket s3path start-ts i]
+  (let [file-name (str s3path "/" (hdfs/file-name hdfs-file) "_" start-ts + "_" i)
         input (hdfs/input-stream hdfs-ctx hdfs-file)
         content-len (hdfs/content-length hdfs-ctx hdfs-file)]
     (prn "load to s3 file " (str s3bucket "/" file-name) content-len)
 
-    (s3/stream->s3! s3-ctx input content-len {:bucket s3bucket :file file-name})
+    (s3/stream->s3! s3-ctx input content-len {:bucket (sanitise-s3-path s3bucket) :file (sanitise-s3-path file-name)})
 
-    (s3/as-s3-fqn (str s3bucket "/" file-name))))
+    (s3/as-s3-fqn (sanitise-s3-path (str s3bucket "/" file-name)))))
 
 (defn pmap2
-  "Run the coll in its own ExecutorService and return the result of (map f coll)"
+  "Run the coll in its own ExecutorService and return the result of (map f coll), f is called with (f start-ts-nanos index coll-item)"
   [threads f coll]
   (let [^ExecutorService exec (Executors/newFixedThreadPool (int threads))
-        ^Callable submit-f (fn [index item] #(f index item))
+        ^Callable submit-f (fn [start-ts index item] #(f start-ts index item))
+        start-ts (System/nanoTime)
         counter-a (atom 0)]
 
     (try
       (map
         deref
         (transduce (comp
-                     (map #(.submit exec (submit-f (swap! counter-a inc) %)))
+                     (map #(.submit exec (submit-f start-ts (swap! counter-a inc) %)))
                      (map #(delay (do
                                     (prn "Waiting on future " %)
                                     (.get ^Future %)))))
@@ -53,7 +63,7 @@
   [hdfs-ctx s3-ctx threads hdfs-dir s3bucket s3path]
   (let [hdfs-files (hdfs/list-paths hdfs-ctx hdfs-dir)]
     (try
-      (pmap2 threads #(hdfs-file->s3 hdfs-ctx %2 s3-ctx s3bucket s3path %1) hdfs-files)
+      (pmap2 threads #(hdfs-file->s3 hdfs-ctx %3 s3-ctx s3bucket s3path %1 %2) hdfs-files)
       (finally
         (prn "Done copying to s3")))))
 
